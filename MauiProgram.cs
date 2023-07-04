c2ba898396209b21df56edeba229bdd7b98e356d
Vexthecollector <82162107+Vexthecollector@users.noreply.github.com>
2023-07-04 16:59:44 +0200

    Fixed Bug that caused the App to not start on Oxygen OS

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -12,13 +12,13 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 		builder.Services.AddSingleton(AudioManager.Current);
-		builder.UseMauiCommunityToolkit();
 
 #if DEBUG
         builder.Logging.AddDebug();
